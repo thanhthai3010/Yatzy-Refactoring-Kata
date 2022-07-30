0eb4748f81cbd98426dc6f0caf3310b8fcf995e6
thaint <thai@emobi.biz>
2022-07-30 20:21:46 +0700

    Small Straight done

--- java/src/main/java/app/Yatzy.java
+++ java/src/main/java/app/Yatzy.java
@@ -1,9 +1,6 @@
 package app;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.OptionalInt;
+import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -137,15 +134,13 @@ public class Yatzy {
 		return 0;
 	}
 
-	public static int smallStraight(int d1, int d2, int d3, int d4, int d5) {
-		int[] tallies;
-		tallies = new int[6];
-		tallies[d1 - 1] += 1;
-		tallies[d2 - 1] += 1;
-		tallies[d3 - 1] += 1;
-		tallies[d4 - 1] += 1;
-		tallies[d5 - 1] += 1;
-		if (tallies[0] == 1 && tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1) return 15;
+	public static int smallStraight(DiceHand diceHand) {
+		List<Integer> sorted = diceHand.stream()
+			.sorted()
+			.collect(toList());
+		if (Arrays.asList(1, 2, 3, 4, 5).equals(sorted)) {
+			return 15;
+		}
 		return 0;
 	}
 
--- java/src/test/java/app/YatzyTest.java
+++ java/src/test/java/app/YatzyTest.java
@@ -99,9 +99,10 @@ public class YatzyTest {
 
 	@Test
 	public void smallStraight() {
-		assertEquals(15, Yatzy.smallStraight(1, 2, 3, 4, 5));
-		assertEquals(15, Yatzy.smallStraight(2, 3, 4, 5, 1));
-		assertEquals(0, Yatzy.smallStraight(1, 2, 2, 4, 5));
+		assertEquals(15, Yatzy.smallStraight(new DiceHand(1, 2, 3, 4, 5)));
+		assertEquals(15, Yatzy.smallStraight(new DiceHand(2, 3, 4, 5, 1)));
+		assertEquals(0, Yatzy.smallStraight(new DiceHand(1, 2, 2, 4, 5)));
+		assertEquals(0, Yatzy.smallStraight(new DiceHand(1, 2, 3, 4, 6)));
 	}
 
 	@Test
